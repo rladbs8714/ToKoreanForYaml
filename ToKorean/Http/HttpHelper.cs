87f61f3964d8972193a1072e8d260929cd6ba5a3
rladbs8714 <rladbs8714@naver.com>
2024-02-06 01:15:09 +0900

    dev HttpHelper
    Http 도우미 개발 중
    당장 필요한 부분만 만드느라 완성도는 높지 않음.
    언젠가 리팩토링 필요.
    
    진행률 25%

---
+++ ToKorean/Http/HttpHelper.cs
@@ -0,0 +1,137 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using ToKorean.Papago;
+
+namespace ToKorean.Http
+{
+    internal class HttpHelper : IDisposable
+    {
+
+        // ==============================================================================
+        // FIELD
+        // ==============================================================================
+
+        /// <summary>
+        /// 해제를 위한 필드 변수
+        /// </summary>
+        private bool disposedValue;
+
+
+        // ==============================================================================
+        // PROPERTY
+        // ==============================================================================
+
+        /// <summary>
+        /// Http 요청자
+        /// </summary>
+        protected HttpClient Client { get; private set; }
+        
+        
+        // ==============================================================================
+        // CONSTRUCTOR
+        // ==============================================================================
+
+        /// <summary>
+        /// 기본 생성자
+        /// </summary>
+        internal HttpHelper() { }
+
+        /// <summary>
+        /// PapagoAPI를 위한 생성자
+        /// </summary>
+        /// <param name="ClientID">PapagoAPI의 Client ID</param>
+        /// <param name="ClientSecret">PapagoAPI의 Client Secret</param>
+        protected HttpHelper(string url, string ClientID, string ClientSecret)
+        {
+            Client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+            Client.DefaultRequestHeaders.Accept.Clear();
+
+            Client.DefaultRequestHeaders.Add("X-Naver-Client-Id", ClientID);
+            Client.DefaultRequestHeaders.Add("X-Naver-Client-Secret", ClientSecret);
+        }
+
+
+        // ==============================================================================
+        // METHOD
+        // ==============================================================================
+
+        /// <summary>
+        /// 비동기로 Post를 진행한다.
+        /// </summary>
+        /// <param name="postContent">Post할 컨텐츠</param>
+        /// <param name="uri">추가할 uri</param>
+        /// <returns>Response된 메시지</returns>
+        internal async Task<string> PostAsync(StringContent postContent, string uri = "")
+        {
+            if (!string.IsNullOrEmpty(uri) &&
+                !Uri.IsWellFormedUriString(uri, UriKind.RelativeOrAbsolute))
+            {
+                Console.WriteLine("Uri 형식이 올바르지 않음.");
+                return string.Empty;
+            }
+
+            using StringContent content = postContent;
+
+            HttpResponseMessage response;
+
+            try
+            {
+                response = await Client.PostAsync(uri, content);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.ToString());
+                return string.Empty;
+            }
+
+            string responseMsg = await response.Content.ReadAsStringAsync();
+
+            response.Dispose();
+            postContent.Dispose();
+
+            return responseMsg;
+        }
+
+        
+        // ==============================================================================
+        // DISPOSABLE
+        // ==============================================================================
+
+        #region DISPOSABLE
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 관리형 상태(관리형 개체)를 삭제합니다.
+                }
+
+                // TODO: 비관리형 리소스(비관리형 개체)를 해제하고 종료자를 재정의합니다.
+                // TODO: 큰 필드를 null로 설정합니다.
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 비관리형 리소스를 해제하는 코드가 'Dispose(bool disposing)'에 포함된 경우에만 종료자를 재정의합니다.
+        // ~HttpHelper()
+        // {
+        //     // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
---
+++ ToKorean/Http/IHttpHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace ToKorean.Http
+{
+    internal interface IHttpHelper
+    {
+
+    }
+}
